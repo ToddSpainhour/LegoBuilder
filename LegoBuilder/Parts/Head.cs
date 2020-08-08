65bc62241c90191a430e4435b037ae8f2f5a10f7
Todd Spainhour <toddspainhour@gmail.com>
2020-08-08 13:18:44 -0500

    classes created

---
+++ LegoBuilder/Parts/Head.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoBuilder
+{
+    class Head
+    {
+
+
+
+
+
+
+        // properties
+
+        public string Hair { get; set; }
+        public string Name { get; set; }
+
+
+
+
+
+
+
+
+        // fields
+
+        public bool _usingBinoculars = false;
+
+
+
+
+
+
+
+
+
+
+        // constructor
+
+        public Head()
+        {
+            //Console.WriteLine("inside your head constructor.");
+        }
+
+
+
+
+
+
+
+
+
+        // methods
+
+        public void MagnifyVision(bool _usingBinoculars)
+        {
+            if (_usingBinoculars)
+            {
+                Console.WriteLine("\"I can see much further when using binoculars,\" he said.");
+            }
+            else
+            {
+                Console.WriteLine("\"I can't see that far,\" he said. \"I should use my binoculars.\"");
+            }
+        }
+
+
+
+    }
+}
---
+++ LegoBuilder/Parts/Legs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoBuilder.Parts
+{
+    class Legs
+    {
+
+        //properties
+        public string LegColor { get; }
+
+
+
+        // fields
+        
+        public string _legColor = "blue";
+
+        // constructors
+
+        public Legs()
+        {
+            //Console.WriteLine("Inside your legs constructor");
+        }
+
+        // methods
+
+        public void Walk (string _legDescription)
+        {
+            if (_legDescription == "blue")
+            {
+                Console.WriteLine($"\"Let's get moving,\" he said as his {_legDescription} legs shuffled down the slope.");
+            }
+            else
+            {
+                Console.WriteLine($"\"Something seems different. Wait my legs are {_legDescription}!\"");
+            }
+        }
+
+    }
+}
