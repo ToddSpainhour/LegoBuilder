e89f6c35cebeee2bc651060ea9e19269ee10e8c0
Todd Spainhour <toddspainhour@gmail.com>
2020-08-07 20:56:49 -0500

    Add project files.

---
+++ LegoBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LegoBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
